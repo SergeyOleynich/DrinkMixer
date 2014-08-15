2c9ed42ea0e05daa4a3d836bac4ff7430e37316e
Sergey Oleynich <sergeyoleynich@gmail.com>
2014-08-15 01:25:56 -0700

    Initial Commit

---
+++ DrinkMixer/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  DrinkMixer
+//
+//  Created by Administrator on 8/15/14.
+//  Copyright (c) 2014 Administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
